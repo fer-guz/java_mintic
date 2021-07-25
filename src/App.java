9aa8ec2e893363d1ead0ef1ae4315a54b97b086a
fer-guz <guzman1044@gmail.com>
2021-07-24 21:02:57 -0500

    second class java code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
+
+
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
     }
 }
