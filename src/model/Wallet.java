ff6ccbc26cdfc724bb874d76ebc06e9633b0d3a3
fer-guz <guzman1044@gmail.com>
2021-08-08 18:22:20 -0500

    funciones extra para wallet

--- src/model/Wallet.java
+++ src/model/Wallet.java
@@ -6,7 +6,7 @@ public class Wallet {
 
     private int saldo;
     private boolean tieneLimite;
-
+    private int meta;
 
     public Wallet() {
 
@@ -28,25 +28,59 @@ public class Wallet {
 
     }
 
-    public String takeMoney(int value){
+    public String takeMoney(int value) {
         if (saldo < value) {
             saldo -= value;
-            return "Transaccion exitosa, nuevo saldo "+ saldo;
+            return "Transaccion exitosa, nuevo saldo " + saldo;
         }
 
         return "saldo insuficiente";
     }
 
-    public String breakLimit(){
+    public String breakLimit() {
         if (saldo >= 10000 && tieneLimite) {
 
             saldo -= 10000;
 
             setTieneLimite(false);
             return "Has roto los limites!!!";
-            
+
         }
         return "no tiene saldo suiciente para romper los limites d tu cuenta :(";
+    }
+
+    public boolean establecerMeta(int valor){
+
+        if(valor == 0){
+            meta = valor;
+            return true;
+
+        }
+        if(valor < 0 || valor <= saldo || valor > CAPACIDAD_MAXIMA && tieneLimite){
+
+            return false;
+        }
+
+        meta = valor;
+        return true;
+    }
+
+    public boolean verificarMeta(){
+        if(meta == 0 || meta > saldo){
+            return false;
+        }else{
+            return true;
+        }
+
+        
+    }
+
+    public int getMeta() {
+        return meta;
+    }
+
+    public void setMeta(int meta) {
+        this.meta = meta;
     }
 
     public int getSaldo() {
